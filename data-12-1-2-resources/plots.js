c9d730e1eb475c6f0f9fbcb2f48194ec71239373
hajaf <hajafatounjie@gmail.com>
2022-07-06 09:07:32 -0400

    class

---
+++ data-12-1-2-resources/plots.js
@@ -0,0 +1,15 @@
+var trace = {
+    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: 'pie'
+   };
+   var data = [trace];
+   var layout = {
+    title: "'Pie' Chart",
+   };
+   Plotly.newPlot("plotArea", data, layout);
+var numbers = [0,2,4,6,8];
+const url = "https://api.spacexdata.com/v2/launchpads";
+
+d3.json(url).then(receivedData => console.log(receivedData));
